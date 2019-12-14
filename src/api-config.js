a69768a3e6e38e758452580fdd6a5f69573d6000
Dustin Sampson <gridcell@gmail.com>
2019-12-13 23:54:37 -0800

    add tile layer subdomains

--- src/api-config.js
+++ src/api-config.js
@@ -1,13 +1,18 @@
 let backendHost
 
+let tileHost
+
 const apiVersion = 'v1'
 
 const hostname = window && window.location && window.location.hostname
 
 if (hostname === 'tiger.speciescl.org') {
   backendHost = 'http://localhost:8181'
+  tileHost = 'http://{s}.localhost:8181'
 } else {
   backendHost = 'http://localhost:8181'
+  tileHost = 'http://localhost:8181'
 }
 
 export const API_ROOT = `${backendHost}/${apiVersion}`
+export const TILE_API_ROOT = `${tileHost}/${apiVersion}`
--- src/lib/maplayers.js
+++ src/lib/maplayers.js
@@ -1,6 +1,6 @@
 import L from 'leaflet'
 import SecureTileLayer from './leaflet-secure-tilelayer'
-import { API_ROOT } from '../api-config'
+import { API_ROOT, TILE_API_ROOT } from '../api-config'
 import SecureApi from './api'
 import TokenUtil from './token'
 
@@ -8,6 +8,11 @@ class MapLayers {
   constructor() {
     this.api = new SecureApi()
     this.tokenUtil = new TokenUtil()
+    this.defaultTileLayerConfig = {
+      attribution: '&copy; WCS',
+      subdomains: ['tilea', 'tileb', 'tilec', 'tiled'],
+      token: this.tokenUtil.token,
+    }
   }
 
   get token() {
@@ -50,69 +55,28 @@ class MapLayers {
   }
 
   getBiomeLayer() {
-    const url = `${API_ROOT}/tiles/biomes/{z}/{x}/{y}/`
+    const url = `${TILE_API_ROOT}/tiles/biomes/{z}/{x}/{y}/`
 
-    return new SecureTileLayer(url, {
-      attribution: '&copy; WCS',
-      token: this.token,
-    })
+    return new SecureTileLayer(url, this.defaultTileLayerConfig)
   }
 
   getProtectedAreaLayer() {
-    const url = `${API_ROOT}/tiles/pas/{z}/{x}/{y}/`
+    const url = `${TILE_API_ROOT}/tiles/pas/{z}/{x}/{y}/`
 
-    return new SecureTileLayer(url, {
-      attribution: '&copy; WCS',
-      token: this.token,
-    })
+    return new SecureTileLayer(url, this.defaultTileLayerConfig)
   }
 
   geHiiLayer() {
-    const url = `${API_ROOT}/tiles/hii/{z}/{x}/{y}/`
+    const url = `${TILE_API_ROOT}/tiles/hii/{z}/{x}/{y}/`
 
-    return new SecureTileLayer(url, {
-      attribution: '&copy; WCS',
-      token: this.token,
-    })
+    return new SecureTileLayer(url, this.defaultTileLayerConfig)
   }
 
   getTigerHistoricalRangeLayer() {
-    const url = `${API_ROOT}/tiles/species/${this.species}/{z}/{x}/{y}/`
+    const url = `${TILE_API_ROOT}/tiles/species/${this.species}/{z}/{x}/{y}/`
 
-    return new SecureTileLayer(url, {
-      attribution: '&copy; WCS',
-      token: this.token,
-    })
+    return new SecureTileLayer(url, this.defaultTileLayerConfig)
   }
 }
-
-// const getTclCountriesBiomesPasLayer = () => {
-//   return L.geoJSON(TclCountriesBiomesPasData, {
-//     style: {
-//       color: '#EB5424',
-//       weight: 1,
-//       strokeOpacity: 0.8,
-//       fillColor: '#EB5424',
-//       fillOpacity: 0.2,
-//     },
-//   }).bindPopup(layer => {
-//     const name = layer.feature.properties.scl_name
-//     const area = parseInt(layer.feature.properties.scl_total_area, 10)
-//     const msg = `
-//       <div>
-//         Name: ${name}
-//       </div>
-//       <div>
-//         Total Area: ${area}km<sup>2</sup>
-//       </div>
-//     `
-
-//     return msg
-//   })
-// }
-
-// const MapLayers = {
-//   getTclCountriesBiomesPasLayer,
-// }
 
 export default MapLayers

4c4fd3a1286664efb844c78dc4fc85c9767aebb8
Nick Hoang <nickhoang11@gmail.com>
2020-03-13 13:34:48 -0700

    Add functional radio form control for static map layers

--- src/components/MapLayerType/index.js
+++ src/components/MapLayerType/index.js
@@ -1,45 +1,44 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-import { makeStyles } from '@material-ui/core/styles'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormControl from '@material-ui/core/FormControl'
 
-const useStyles = makeStyles(theme => ({
-  formControl: {
-    margin: theme.spacing(3),
-  },
-}))
+import usePrevious from '../../lib/usePrevious'
 
 const MapLayerType = ({ map, layers }) => {
-  const classes = useStyles()
-  const [value, setValue] = useState('')
-
-  const prevValueRef = useRef()
-
-  useEffect(() => {
-    prevValueRef.current = value
-  })
-  const prevValue = prevValueRef.current
+  const layerNames = Object.keys(layers)
+  const [radioValue, setRadioValue] = useState('None')
+  const prevRadioValue = usePrevious(radioValue)
 
   const handleChange = event => {
-    const selectedLayer = event.target.value
-
-    if (prevValue !== '') {
-      map.current.removeLayer(layers[prevValue])
-    }
-    map.current.addLayer(layers[selectedLayer])
-    setValue(selectedLayer)
+    setRadioValue(event.target.value)
   }
 
+  useEffect(() => {
+    if (radioValue === "None") {
+      if (prevRadioValue !== '' && prevRadioValue !== undefined) {
+        map.current.removeLayer(layers[prevRadioValue])
+      }
+    } else if (prevRadioValue === "None") {
+      map.current.addLayer(layers[radioValue])
+    } else {
+      map.current.removeLayer(layers[prevRadioValue])
+      map.current.addLayer(layers[radioValue])
+    }
+  }, [radioValue])
+
+  const layerOptions = layerNames.map(name => {
+    return <FormControlLabel key={name} value={name} control={<Radio />} label={name}/>
+  })
+
   return (
-    <FormControl component="fieldset" className={classes.formControl}>
-      <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-        <FormControlLabel value="Protected Area" control={<Radio />} label="Protected Area" />
-        <FormControlLabel value="Biome" control={<Radio />} label="Biome" />
-        <FormControlLabel value="Human Influence Index" control={<Radio />} label="Human Influence Index" />
+    <FormControl component="fieldset">
+      <RadioGroup aria-label="gender" name="gender1" value={radioValue} onChange={handleChange}>
+        {layerOptions}
+        <FormControlLabel key="None" value="None" control={<Radio />} label="None"/>
       </RadioGroup>
     </FormControl>
   )
